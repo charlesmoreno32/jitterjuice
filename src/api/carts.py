005655a2656252fc72b6a904bb5e9dce3e3f1182
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-10-06 11:28:24 -0700

    string conc

--- src/api/carts.py
+++ src/api/carts.py
@@ -63,17 +63,17 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
         if(item_sku == "RED_POTION_0"):
             num_red += cart[item_sku]
             with db.engine.begin() as connection:
-                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + cart[item_sku]))
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + str(cart[item_sku])))
                 connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold + " + str(num_red * 50)))
         elif(item_sku == "GREEN_POTION_0"):
             num_red += cart[item_sku]
             with db.engine.begin() as connection:
-                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + cart[item_sku]))
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + str(cart[item_sku])))
                 connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold + " + str(num_red * 50)))
         elif(item_sku == "RED_POTION_0"):
             num_red += cart[item_sku]
             with db.engine.begin() as connection:
-                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + cart[item_sku]))
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + str(cart[item_sku])))
                 connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold + " + str(num_red * 50)))
     tot_pots = num_red + num_green + num_blue
     return {"total_potions_bought": tot_pots, "total_gold_paid": tot_pots * 50}
