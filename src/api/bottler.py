54bfc73c0a9274a6c85aecb4400f228819e7d4aa
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-10-17 11:20:13 -0700

    updated bottler plan

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -86,7 +86,7 @@ def get_bottle_plan():
     while(inventory < 300 and times < count):
         times = 0
         for potion in potion_lst:
-            if(inventory < 300 and potion.potion_type[0] < red_ml and potion.potion_type[1] < green_ml and potion.potion_type[2] < blue_ml and potion.potion_type[3] < dark_ml):
+            if(inventory < 300 and potion.potion_type[0] <= red_ml and potion.potion_type[1] <= green_ml and potion.potion_type[2] <= blue_ml and potion.potion_type[3] <= dark_ml):
                 red_ml -= potion.potion_type[0]
                 green_ml -= potion.potion_type[1]
                 blue_ml -= potion.potion_type[2]
@@ -112,4 +112,3 @@ def get_bottle_plan():
             }
             ]"""
     return plan
-get_bottle_plan()
