ec6ff3dd2a98b3a7fd0285f5f14205511f0c02a6
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-10-21 13:55:21 -0700

    implemented ledger

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,6 +16,24 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("""
+                                           TRUNCATE cart_items, carts, globals, gold_ledger, ml_ledger, potion_ledger
+                                           """))
+        connection.execute(sqlalchemy.text("""
+                                           INSERT INTO gold_ledger (gold_change)
+                                           VALUES (100)
+                                           """))
+        connection.execute(sqlalchemy.text("""
+                                           INSERT INTO ml_ledger (red_ml_change, green_ml_change, blue_ml_change, dark_ml_change)
+                                           VALUES (0, 0, 0, 0)
+                                           """))
+        connection.execute(sqlalchemy.text("""
+                                           INSERT INTO potions_ledger (potion_change, potion_id)
+                                           SELECT (0, potions.id)
+                                           FROM potions
+                                           GROUP BY potion_id
+                                           """))
     
     return "OK"
 
